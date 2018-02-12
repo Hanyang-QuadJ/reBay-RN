02dbe704b57640cf429f29e454dfcead182b2bd3
ShinHyunJong <kashiashin@gmail.com>
2018-02-12 22:26:30 +0900

    Tutorial

--- App.js
+++ App.js
@@ -1,27 +1,60 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Provider} from 'react-redux';
-import { StackNavigator } from 'react-navigation';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Provider} from 'react-redux';
+import {StackNavigator, TabNavigator, TabBarBottom} from 'react-navigation';
 import store from './store';
+import {Ionicons} from '@expo/vector-icons';
 import introScreen from './screens/introScreen';
+import mainScreen from './screens/mainScreen';
+import sellScreen from './screens/sellScreen';
 
 export default class App extends Component {
 
-  render() {
+    render() {
+        const TabNavigation = TabNavigator({
+            Home: {screen: mainScreen},
+            Sell: {screen: sellScreen}
+        });
 
-      const MainNavigator = StackNavigator({
-          Main : { screen: introScreen},
 
-      },{
-          headerMode:'none'
-      });
-    return (
-        <Provider store={store}>
-            <MainNavigator/>
-        </Provider>
+        const MainNavigator = StackNavigator({
+            Intro: {screen: introScreen},
+            Home: {screen: TabNavigation}
+        },
+            {
+                navigationOptions: ({ navigation }) => ({
+                    tabBarIcon: ({ focused, tintColor }) => {
+                        const { routeName } = navigation.state;
+                        let iconName;
+                        if (routeName === 'Home') {
+                            iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+                        } else if (routeName === 'Settings') {
+                            iconName = `ios-options${focused ? '' : '-outline'}`;
+                        }
 
-    );
-  }
+                        // You can return any component that you like here! We usually use an
+                        // icon component from react-native-vector-icons
+                        return <Ionicons name={iconName} size={25} color={tintColor} />;
+                    },
+                }),
+                tabBarOptions: {
+                    activeTintColor: 'tomato',
+                    inactiveTintColor: 'gray',
+                },
+                tabBarComponent: TabBarBottom,
+                tabBarPosition: 'bottom',
+                animationEnabled: false,
+                swipeEnabled: false,
+            });
+
+
+        return (
+            <Provider store={store}>
+                <MainNavigator/>
+            </Provider>
+
+        );
+    }
 }
 
 
--- screens/IntroScreen.js
+++ screens/IntroScreen.js
@@ -1,49 +1,106 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {StyleSheet, Text, View, Platform, TouchableHighlight} from 'react-native';
-import { STATUS_BAR_HEIGHT } from '../constants';
+import {Button} from 'react-native-elements';
+import {STATUS_BAR_HEIGHT} from '../constants';
 import Swiper from 'react-native-swiper';
 
 class introScreen extends Component {
     static navigationOptions = () => ({
+        header: null
 
     });
-    constructor(props){
-        super(props)
-        this.state = { count: 0}
+
+    constructor(props) {
+        super(props);
+        this.state = {count: 0}
 
     }
-    onPress= () =>{
+
+    onPress = () => {
         this.setState({
-            count:this.state.count + 1
+            count: this.state.count + 1
         })
     }
 
     render() {
         return (
-            <Swiper style={styles.wrapper} showsButtons={false}>
-                <View style={styles.slide1}>
-                    <Text style={styles.text}>Hello Swiper</Text>
+            <View style={{flex: 1}}>
+                <Swiper style={styles.wrapper} showsButtons={false} dot={<View style={{
+                    backgroundColor: 'rgba(0,0,0,.2)',
+                    width: 8, height: 8, borderRadius: 4, marginLeft: 3, marginRight: 3, marginTop: 0, marginBottom: 150
+                }}/>}
+                        activeDot={<View style={{
+                            backgroundColor: '#007aff', width: 8, height: 8, borderRadius: 4, marginLeft: 3,
+                            marginRight: 3, marginTop: 0, marginBottom: 150,
+                        }}/>}
+                >
+                    <View style={styles.slide1}>
+                        <Text style={styles.text}>튜토리얼1</Text>
+                    </View>
+                    <View style={styles.slide2}>
+                        <Text style={styles.text}>튜토리얼2</Text>
+                    </View>
+                    <View style={styles.slide3}>
+                        <Text style={styles.text}>튜토리얼3</Text>
+                    </View>
+                </Swiper>
+                <View style={styles.staticJump}>
+                    <TouchableHighlight  onPress={() => this.props.navigation.navigate('Home')}>
+                        <Text style={styles.staticJumpText}>건너뛰기</Text>
+                    </TouchableHighlight>
                 </View>
-                <View style={styles.slide2}>
-                    <Text style={styles.text}>Beautiful</Text>
+                <View style={styles.staticAuth}>
+                    <Button title='로그인' onPress={() => this.props.navigation.navigate('Home')}
+                            buttonStyle={{
+                                backgroundColor: "rgba(92, 99,216, 1)",
+                                height:45,
+                                borderColor: "transparent",
+                                borderWidth: 0,
+                                borderRadius: 5,
+                            }}/>
+                    <Button title='회원가입' onPress={() => this.props.navigation.navigate('Home')}
+                            buttonStyle={{
+                                marginTop:10,
+                                backgroundColor: "rgba(92, 99,216, 0.5)",
+                                height:45,
+                                borderColor: "transparent",
+                                borderWidth: 0,
+                                borderRadius: 5,
+                            }}/>
                 </View>
-                <View style={styles.slide3}>
-                    <Text style={styles.text}>And simple</Text>
-                </View>
-            </Swiper>
+            </View>
+
+
         )
 
     }
 
 }
+
 const styles = StyleSheet.create({
-    wrapper: {
-    },
+    wrapper: {},
     slide1: {
         flex: 1,
+        backgroundColor: '#4c586f',
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#9DD6EB',
+    },
+    staticJump:{
+        position: 'absolute',
+        zIndex: 100,
+        right: 10,
+        top: 30,
+    },
+    staticJumpText:{
+        fontSize:16,
+
+    },
+    staticAuth: {
+        position: 'absolute',
+        zIndex: 100,
+        left: 20,
+        right: 20,
+        bottom: 30,
     },
     slide2: {
         flex: 1,
@@ -61,6 +118,7 @@ const styles = StyleSheet.create({
         color: '#fff',
         fontSize: 30,
         fontWeight: 'bold',
-    }
+    },
+    button: {}
 })
 export default introScreen;

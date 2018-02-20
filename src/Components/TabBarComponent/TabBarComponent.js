0b66daec6350cd823a79acf14cf8ff664ba7322e
ShinHyunJong <kashiashin@gmail.com>
2018-02-20 17:25:25 +0900

    routing

--- src/Components/TabBarComponent/TabBarComponent.js
+++ src/Components/TabBarComponent/TabBarComponent.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import {View, TouchableWithoutFeedback, Text , AsyncStorage, Alert} from 'react-native';
-import{ Ionicons }from '@expo/vector-icons';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {View, TouchableWithoutFeedback, Text, AsyncStorage, Alert} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
+import {NavigationActions} from 'react-navigation'
+import {connect} from 'react-redux';
 import styles from "./Style";
 import * as commonStyle from "../../Constants/commonStyle";
 import * as ScrollToTopCreator from '../../ActionCreators/ScrollToTopCreator';
@@ -11,15 +12,14 @@ const activeTintColor = commonStyle.PRIMARY_COLOR;
 const inactiveTintColor = commonStyle.BORDER_COLOR;
 
 const mapStateToProps = state => {
-    return {
-    };
+    return {};
 };
 
 
-class TabBarComponent extends Component{
+class TabBarComponent extends Component {
 
     renderItem = (route, index) => {
-        const { navigation, jumpToIndex } = this.props;
+        const {navigation, jumpToIndex} = this.props;
 
         const focused = index === navigation.state.index;
         const color = focused ? activeTintColor : inactiveTintColor;
@@ -54,42 +54,57 @@ class TabBarComponent extends Component{
                 style={styles.tab}
                 onPress={() => {
                     switch (route.routeName) {
-                        case "Home" : return focused ? this.props.navigation.dispatch(ScrollToTopCreator.scrollToTop()) : jumpToIndex(index)
-                        case "Buy" : return focused ? console.log('check2') : jumpToIndex(index);
-                        case "Sell" : AsyncStorage.getItem("ACCESS_TOKEN").then(value => {
-                            if(value === "" || value === undefined || value === null){
-                                return Alert.alert(
-                                    '회원가입이 필요합니다!',
-                                    '계속 진행하시려면 확인을 누르세요',
-                                    [
-                                        {text: '취소', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
-                                        {text: '확인', onPress: () => this.props.navigation.navigate('Terms')},
-                                    ],
-                                    { cancelable: false }
-                                )
-                            }
-                            else{
-                                return navigation.navigate('SellStack');
-                            }
+                        case "Home" :
+                            return focused ? this.props.navigation.dispatch(ScrollToTopCreator.scrollToTop()) : jumpToIndex(index)
+                        case "Buy" :
+                            return focused ? console.log('check2') : jumpToIndex(index);
+                        case "Sell" :
+                            AsyncStorage.getItem("ACCESS_TOKEN").then(value => {
+                                if (value === "" || value === undefined || value === null) {
+                                    Alert.alert(
+                                        '회원가입이 필요합니다!',
+                                        '계속 진행하시려면 확인을 누르세요',
+                                        [
+                                            {text: '취소', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                                            {
+                                                text: '확인', onPress: () => this.props.navigation.dispatch(
+                                                    NavigationActions.reset({
+                                                        index: 0,
+                                                        key: null,
+                                                        actions: [
+                                                            NavigationActions.navigate({ routeName: 'Auth' })
+                                                        ]
+                                                    })
+                                                )
+                                            },
+                                        ],
+                                        {cancelable: false}
+                                    )
+                                }
+                                else {
+                                    return navigation.navigate('SellStack');
+                                }
 
-                        });
+                            });
                             break;
 
-
-                        case "Notice" : return focused ? console.log('check2') : jumpToIndex(index);
-                        case "Profile" : return focused ? console.log('check2') : jumpToIndex(index);
-                        default: return jumpToIndex(index);
+                        case "Notice" :
+                            return focused ? console.log('check2') : jumpToIndex(index);
+                        case "Profile" :
+                            return focused ? console.log('check2') : jumpToIndex(index);
+                        default:
+                            return jumpToIndex(index);
                     }
                 }}
             >
                 {route.routeName === "Sell" ?
                     <View style={styles.sellTab}>
                         <Ionicons size={24} name={iconName} color="white"/>
-                        <Text style={{color:"white"}}>{tabName}</Text>
+                        <Text style={{color: "white"}}>{tabName}</Text>
                     </View> :
                     <View style={styles.tab}>
                         <Ionicons size={24} name={iconName} color={color}/>
-                        <Text style={{color:color, fontSize:12}}>{tabName}</Text>
+                        <Text style={{color: color, fontSize: 12}}>{tabName}</Text>
                     </View>
                 }
 
@@ -98,9 +113,9 @@ class TabBarComponent extends Component{
     };
 
     render() {
-        const { navigation } = this.props;
+        const {navigation} = this.props;
 
-        const { routes } = navigation.state;
+        const {routes} = navigation.state;
 
         return (
             <View style={styles.tabBar}>
@@ -108,8 +123,8 @@ class TabBarComponent extends Component{
             </View>
         );
     }
-    
-    
-    
+
+
 }
+
 export default (TabBarComponent = connect(mapStateToProps)(TabBarComponent));

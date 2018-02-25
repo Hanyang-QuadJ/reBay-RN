6eafb203079efc3ab587df74a1162034be486f7c
JeongHyunLee <nomar0107@gmail.com>
2018-02-25 17:08:00 +0900

    구매창에서 브랜드 검색하기

--- src/Screens/TabScreens/BuyScreen/BuyScreen.js
+++ src/Screens/TabScreens/BuyScreen/BuyScreen.js
@@ -1,34 +1,80 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Container, Text, Content, Item, Input} from 'native-base';
+import {AsyncStorage, View, FlatList} from 'react-native';
+import { Col, Row, Grid } from 'react-native-easy-grid';
+import {Container, Text, Content, Item, Input, Form, Button, Badge, Header, Footer, Title, Left, Right, List, ListItem} from 'native-base';
 import HeaderComponent from '../../../Components/HeaderComponent/HeaderComponent'
 import styles from './Style';
+import * as BuyActionCreator from '../../../ActionCreators/BuyActionCreator';
+import * as BrandActionCreator from "../../../ActionCreators/BrandActionCreator";
 
 const mapStateToProps = state => {
-    return {};
+    return {
+        brand:state.BrandReducer.brand
+    };
 };
 
 class BuyScreen extends Component {
     constructor(props) {
         super(props);
+        this.state = {text: '',brandData:[]};
+    }
+
+    componentWillMount(){
+        AsyncStorage.getItem("ACCESS_TOKEN").then(token => {
+            this.props.dispatch(BrandActionCreator.getBrand(token));
+        });
     }
 
     componentDidMount() {
 
     }
-
-    render() {
-        console.log("BuyScreen");
+    filterBrandwithSearch() {
+        const brands = [];
+        if (this.props.brand != null) {
+            if(this.state.text!= null && this.state.text.length >0) {
+                const text = this.state.text;
+                this.props.brand.brands.forEach(function (val, index) {
+                    if (val.brand_name.indexOf(text) !== -1) {
+                        brands.push(val);
+                    }
+                })
+            }
+            else {
+                this.props.brand.brands.forEach(function (val, index) {
+                        brands.push(val);
+                })
+            }
+        }
         return (
-            <Container style={{flex: 1, backgroundColor: '#ddd'}}>
-                <HeaderComponent title="구매하기" left="" right="ios-basket"/>
-				<Content>
-					<Text>브랜드 검색</Text>
-		        	<Item rounded style={styles.itemStyle}>
-		        		<Input placeholder='Rounded Textbox'/>
-		        	</Item>
-		        </Content>
-            </Container>
+            <FlatList
+                data={brands}
+                renderItem={({item}) => <Text>{item.brand_name}</Text>}
+            />
+        )
+    }
+    render() {
+
+        console.log("BuyScreen");
+        console.log(this.props.brand);
+        console.log(this.state.text);
+
+
+
+        return (
+                <View>
+                    <HeaderComponent title="구매하기" left="" right="ios-basket"/>
+                    <Text>브랜드 검색</Text>
+                    <Item rounded style={styles.itemStyle}>
+                        <Input placeholder='Rounded Textbox' onChangeText={(text) => this.setState({text})}/>
+                    </Item>
+                    {
+                        this.filterBrandwithSearch()
+                    }
+                </View>
+
+
+
         )
     }
 
--- src/Screens/TabScreens/BuyScreen/Style.js
+++ src/Screens/TabScreens/BuyScreen/Style.js
@@ -3,6 +3,15 @@ const style = StyleSheet.create({
     itemStyle: {
 		height: 30,
 		borderColor: 'black'
-	}
+	},
+    alphabetWrapper: {
+        flexWrap: 'wrap',
+        alignItems: 'flex-start',
+        flexDirection:'row',
+    },
+    //non-container style wrapper for scrollview
+    footerWrapperNC: {
+        flexDirection:'column',
+    },
 });
 export default style

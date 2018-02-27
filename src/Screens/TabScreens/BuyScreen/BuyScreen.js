a762c5111b4b0c399af86c7a01f91adedbe69981
JeongHyunLee <nomar0107@gmail.com>
2018-02-27 15:58:41 +0900

    정현 버이 페이지 검색까지함

--- src/Screens/TabScreens/BuyScreen/BuyScreen.js
+++ src/Screens/TabScreens/BuyScreen/BuyScreen.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {AsyncStorage, View, FlatList} from 'react-native';
 import { Col, Row, Grid } from 'react-native-easy-grid';
-import {Container, Text, Content, Item, Input, Form, Button, Badge, Header, Footer, Title, Left, Right, List, ListItem} from 'native-base';
+import {Container, Text, Content, Item, Input, Form, Button, Badge, Header, Footer, Title, Left, Right, List, ListItem,FooterTab} from 'native-base';
 import HeaderComponent from '../../../Components/HeaderComponent/HeaderComponent'
 import styles from './Style';
 import * as BuyActionCreator from '../../../ActionCreators/BuyActionCreator';
@@ -17,67 +17,65 @@ const mapStateToProps = state => {
 class BuyScreen extends Component {
     constructor(props) {
         super(props);
-        this.state = {text: '',brandData:[]};
+        this.state = {
+            searchBarInput: '',
+            currentBrand:[]
+        };
     }
 
     componentWillMount(){
         AsyncStorage.getItem("ACCESS_TOKEN").then(token => {
             this.props.dispatch(BrandActionCreator.getBrand(token));
         });
-    }
-
-    componentDidMount() {
 
     }
-    filterBrandwithSearch() {
+
+    componentWillReceiveProps(nextProps){
+        if(nextProps.brand != null){
+            this.setState({
+                currentBrand: nextProps.brand.brands
+            });
+        }
+    }
+
+    filterBySearchBar(text){
         const brands = [];
         if (this.props.brand != null) {
-            if(this.state.text!= null && this.state.text.length >0) {
-                const text = this.state.text;
-                this.props.brand.brands.forEach(function (val, index) {
-                    if (val.brand_name.indexOf(text) !== -1) {
-                        brands.push(val);
-                    }
-                })
-            }
-            else {
-                this.props.brand.brands.forEach(function (val, index) {
-                        brands.push(val);
-                })
-            }
+            this.props.brand.brands.forEach(function (val, index) {
+                if (val.brand_name.indexOf(text) !== -1) {
+                    brands.push(val);
+                }
+            })
         }
-        return (
-            <FlatList
-                data={brands}
-                renderItem={({item}) => <Text>{item.brand_name}</Text>}
-            />
-        )
+        this.setState({
+            currentBrand: brands
+        });
     }
-    render() {
 
+    render() {
         console.log("BuyScreen");
         console.log(this.props.brand);
         console.log(this.state.text);
-
-
-
         return (
-                <View>
+                <Container>
                     <HeaderComponent title="구매하기" left="" right="ios-basket"/>
                     <Text>브랜드 검색</Text>
                     <Item rounded style={styles.itemStyle}>
-                        <Input placeholder='Rounded Textbox' onChangeText={(text) => this.setState({text})}/>
+                        <Input placeholder='Rounded Textbox' onChangeText={(text) => this.filterBySearchBar(text)}/>
                     </Item>
-                    {
-                        this.filterBrandwithSearch()
-                    }
-                </View>
+                    <FlatList
+                        data={this.state.currentBrand}
+                        renderItem={({item}) => <Text>{item.brand_name}</Text>}
+                    />
+                    <Footer>
+                            <Button full>
+                                <Text>선택하기</Text>
+                            </Button>
+                    </Footer>
 
-
-
+                </Container>
         )
     }
-
 }
 
 export default (BuyScreen = connect(mapStateToProps)(BuyScreen));

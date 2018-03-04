7439265a23707429879a38d27f2c690ab57dee08
JeongHyunLee <nomar0107@gmail.com>
2018-03-04 19:27:50 +0900

    아이템 액션 크리에이터에 18개씩 아이템 가져오는거 추가

--- src/ActionCreators/ItemActionCreator.js
+++ src/ActionCreators/ItemActionCreator.js
@@ -9,12 +9,17 @@ export const SUCCEED_TO_GET_ITEM = "SUCCEED_TO_GET_ITEM";
 //아이템 사진 가져오기
 export const START_TO_GET_ITEM_PICTURE = "START_TO_GET_ITEM_PICTURE";
 export const FAILED_TO_GET_ITEM_PICTURE = "FAILED_TO_GET_ITEM_PICTURE";
-export const SUCCEED_TO_GET_ITEM_PICTURE ="SUCCEED_TO_GET_ITEM_PICTURE";
+export const SUCCEED_TO_GET_ITEM_PICTURE = "SUCCEED_TO_GET_ITEM_PICTURE";
 
 //아이템 등록하기
 export const START_TO_POST_ITEM = "START_TO_POST_ITEM";
 export const FAILED_TO_POST_ITEM = "FAILED_TO_POST_ITEM";
 export const SUCCEED_TO_POST_ITEM = "SUCCEED_TO_POST_ITEM";
+
+//아이템 걸러서 여러개 가져오기
+export const START_TO_POST_ITEMS = "START_TO_POST_ITEM";
+export const FAILED_TO_POST_ITEMS = "FAILED_TO_POST_ITEM";
+export const SUCCEED_TO_POST_ITEMS = "SUCCEED_TO_POST_ITEM";
 
 const ACCESS_TOKEN = "ACCESS_TOKEN";
 
@@ -25,7 +30,7 @@ export const getItem = (token, id) => {
         try {
             await dispatch({type: START_TO_GET_ITEM});
             let response = await fetch(
-                ServerEndPoint2 + "api/item/one/"+id, {
+                ServerEndPoint2 + "api/item/one/" + id, {
                     method: 'GET',
                     headers: {
                         Accept: 'application/json',
@@ -51,7 +56,7 @@ export const getItemPicture = (token, id) => {
         try {
             await dispatch({type: START_TO_GET_ITEM_PICTURE});
             let response = await fetch(
-                ServerEndPoint2 + "api/item/pic/"+id, {
+                ServerEndPoint2 + "api/item/pic/" + id, {
                     method: 'GET',
                     headers: {
                         Accept: 'application/json',
@@ -134,4 +139,39 @@ export const postItem = (token,
 
     }
 
+};
+
+export const postItems = (token, category_1, category_2, item_status, season, max_price, min_price, index) => {
+
+    return async (dispatch) => {
+        try {
+            await dispatch({type: START_TO_POST_ITEMS});
+            let response = await fetch(
+                ServerEndPoint2 + "api/search", {
+                    method: 'POST',
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                        'x-access-token': token
+                    },
+                    body: JSON.stringify({
+                        category_1: category_1,
+                        category_2: category_2,
+                        item_status: item_status,
+                        season: season,
+                        max_price: max_price,
+                        min_price: min_price,
+                        index: index,
+                    }),
+                }
+            );
+            let responseJson = await response.json();
+            dispatch({type: SUCCEED_TO_POST_ITEMS, payload: responseJson});
+            return responseJson;
+        } catch (error) {
+            dispatch({type: FAILED_TO_POST_ITEMS, payload: {data: "NETWORK_ERROR"}});
+            console.error(error);
+        }
+
+    }
 };
